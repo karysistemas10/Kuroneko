e840dec4f7be1e41ccb1b4407f56bf0f1bc3adc5
karymartinez10 <karysistemas10@gmail.com>
2022-09-29 09:46:50 -0500

    Se agregan varios archivos a rama javascript

--- custom.js
+++ custom.js
@@ -1,4 +1,3 @@
-
 var loader;
 
 function loadNow(opacity) {
@@ -21,3 +20,37 @@ document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loader');
     loadNow(1);
 });
+
+(function(){
+    $(document).ready(function(){
+        $('.alt-form').click(function(){
+            $('.form-content').animate({
+                height: "toggle",
+                opacity: 'toggle'
+            }, 600);
+        });
+
+        let formRegistro = document.getElementsByName('form-input');
+        for (let i = 0; i < formRegistro.length; i++) {
+            formRegistro[i].addEventListener('blur', function(){
+                if (this.value.length >= 1) {
+                    this.nextElementSibling.classList.add('active');
+                    this.nextElementSibling.classList.remove('error');
+                } else if (this.value.length = " ") {
+                    this.nextElementSibling.classList.add('error');
+                    this.nextElementSibling.classList.remove('active');
+                } else {
+                    this.nextElementSibling.classList.remove('active');
+                }
+            })
+        }
+
+    })
+}())
+
+
+/*adoptar*/
+function hacer_click()
+{
+    alert("Llena el siguiente formulario para empezar el registro de adopción");
+}
